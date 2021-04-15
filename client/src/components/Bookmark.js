4ded7de7e0fdc3238ee7a718801da9b88d0de8c6
abhasp <abhas0906@gmail.com>
2021-04-15 14:06:22 -0600

    Added home page component from template

--- client/src/components/Bookmark.js
+++ client/src/components/Bookmark.js
@@ -28,6 +28,98 @@ export default class Bookmark extends Component{
                         </div>
                     </Card.Content>
                 </Card>
+                <Card>
+                    <Card.Content>
+                        <Image
+                            floated='right'
+                            size='mini'
+                            src='/images/avatar/large/steve.jpg'
+                        />
+                        <Card.Header>Steve Sanders</Card.Header>
+                        <Card.Meta>Friends of Elliot</Card.Meta>
+                        <Card.Description>
+                            Steve wants to add you to the group <strong>best friends</strong>
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra centered>
+                        <div className='ui three buttons' textAlign="center">
+                            <Button.Group centered>
+                                <Button >View</Button>
+                                <Button>Edit</Button>
+                                <Button>Delete</Button>
+                            </Button.Group>
+                        </div>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Card.Content>
+                        <Image
+                            floated='right'
+                            size='mini'
+                            src='/images/avatar/large/steve.jpg'
+                        />
+                        <Card.Header>Steve Sanders</Card.Header>
+                        <Card.Meta>Friends of Elliot</Card.Meta>
+                        <Card.Description>
+                            Steve wants to add you to the group <strong>best friends</strong>
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra centered>
+                        <div className='ui three buttons' textAlign="center">
+                            <Button.Group centered>
+                                <Button >View</Button>
+                                <Button>Edit</Button>
+                                <Button>Delete</Button>
+                            </Button.Group>
+                        </div>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Card.Content>
+                        <Image
+                            floated='right'
+                            size='mini'
+                            src='/images/avatar/large/steve.jpg'
+                        />
+                        <Card.Header>Steve Sanders</Card.Header>
+                        <Card.Meta>Friends of Elliot</Card.Meta>
+                        <Card.Description>
+                            Steve wants to add you to the group <strong>best friends</strong>
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra centered>
+                        <div className='ui three buttons' textAlign="center">
+                            <Button.Group centered>
+                                <Button >View</Button>
+                                <Button>Edit</Button>
+                                <Button>Delete</Button>
+                            </Button.Group>
+                        </div>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Card.Content>
+                        <Image
+                            floated='right'
+                            size='mini'
+                            src='/images/avatar/large/steve.jpg'
+                        />
+                        <Card.Header>Steve Sanders</Card.Header>
+                        <Card.Meta>Friends of Elliot</Card.Meta>
+                        <Card.Description>
+                            Steve wants to add you to the group <strong>best friends</strong>
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra centered>
+                        <div className='ui three buttons' textAlign="center">
+                            <Button.Group centered>
+                                <Button >View</Button>
+                                <Button>Edit</Button>
+                                <Button>Delete</Button>
+                            </Button.Group>
+                        </div>
+                    </Card.Content>
+                </Card>
             </Card.Group>
         )
     }
