2d9cd9772635e7de06ef98666ba7952c08ae603b
Casecarsid <sisu7408@colorado.edu>
2021-04-18 23:18:22 -0600

    To add rabbitmq service for bookmarks
    Signed-off-by: Casecarsid <sisu7408@colorado.edu>

--- controller/controller.go
+++ controller/controller.go
@@ -36,6 +36,8 @@ func failOnError(err error, msg string) {
 }
 
 func send(bookmark model.Bookmark){
+	//rabbitMQHost := os.Getenv("RABBITMQ_SERVICE_HOST")
+	//conn, err := amqp.Dial(rabbitMQHost)
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
